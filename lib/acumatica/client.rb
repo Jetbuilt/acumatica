26eb1bfdcd784cf2f5c2233fc7798cf90ac65985
Jared Moody <jared@jaredmoody.com>
2018-03-02 19:20:29 -0800

    Add top level module

--- lib/acumatica/client.rb
+++ lib/acumatica/client.rb
@@ -1,43 +1,45 @@
-class Acumatica::Client
-  include Singleton
+module Acumatica
+  class Client
+    include Singleton
 
-  API_VERSION = "6.00.001".freeze
+    API_VERSION = "6.00.001".freeze
 
-  attr_accessor :url, :name, :password, :token
+    attr_accessor :url, :name, :password, :token
 
-  def self.configure
-    yield(instance)
-    instance
-  end
+    def self.configure
+      yield(instance)
+      instance
+    end
 
-  def connection
-    @connection ||= Faraday.new do |conn|
-      conn.request :json
+    def connection
+      @connection ||= Faraday.new do |conn|
+        conn.request :json
 
-      if token.present?
-        conn.request :oauth2, token
-      else
-        conn.use :cookie_jar
+        if token.present?
+          conn.request :oauth2, token
+        else
+          conn.use :cookie_jar
+        end
+
+        conn.response :json
+        conn.adapter Faraday.default_adapter
       end
-
-      conn.response :json
-      conn.adapter Faraday.default_adapter
     end
-  end
 
-  def login
-    response = connection.post do |req|
-      req.url URI.join(@url, "/entity/auth/login")
-      req.body = { name: @name, password: @password }
+    def login
+      response = connection.post do |req|
+        req.url URI.join(@url, "/entity/auth/login")
+        req.body = { name: @name, password: @password }
+      end
+      response.success?
     end
-    response.success?
-  end
 
-  def logout
-    connection.post(URI.join(@url, "/entity/auth/logout")).success?
-  end
+    def logout
+      connection.post(URI.join(@url, "/entity/auth/logout")).success?
+    end
 
-  def stock_items
-    Acumatica::StockItem
+    def stock_items
+      Acumatica::StockItem
+    end
   end
 end
--- lib/acumatica/stock_item.rb
+++ lib/acumatica/stock_item.rb
@@ -1,44 +1,46 @@
-class Acumatica::StockItem < OpenStruct
-  def self.find_all(select: nil, filter: nil, expand: nil, offset: nil, limit: nil)
-    params = {}
+module Acumatica
+  class StockItem < OpenStruct
+    def self.find_all(select: nil, filter: nil, expand: nil, offset: nil, limit: nil)
+      params = {}
 
-    params['$select'] = select if select
-    params['$filter'] = filter if filter
-    params['$expand'] = expand if expand
-    params['$offset'] = offset if offset
-    params['$top']    = limit  if limit
+      params['$select'] = select if select
+      params['$filter'] = filter if filter
+      params['$expand'] = expand if expand
+      params['$offset'] = offset if offset
+      params['$top']    = limit  if limit
 
-    response = Acumatica::Client.instance.connection.get do |req|
-      req.url url
-      req.params = params
+      response = Acumatica::Client.instance.connection.get do |req|
+        req.url url
+        req.params = params
+      end
+
+      response.body.map { |attrs| self.new(attrs) }
     end
 
-    response.body.map { |attrs| self.new(attrs) }
-  end
-
-  def self.url
-    URI.join(
-      Acumatica::Client.instance.url,
-      "/entity/Default/#{Acumatica::Client::API_VERSION}/StockItem"
-    )
-  end
-
-  def initialize(params = {})
-    new_params = {}
-    params.each do |k,v|
-      v = v.values.first if v.is_a?(Hash) && v.keys == ["value"]
-      new_params[methodify(k)] = v
+    def self.url
+      URI.join(
+        Acumatica::Client.instance.url,
+        "/entity/Default/#{Acumatica::Client::API_VERSION}/StockItem"
+      )
     end
 
-    super(new_params)
+    def initialize(params = {})
+      new_params = {}
+      params.each do |k,v|
+        v = v.values.first if v.is_a?(Hash) && v.keys == ["value"]
+        new_params[methodify(k)] = v
+      end
 
-    self.attributes ||= []
-    attributes.each { |a| self[methodify(a['AttributeID']['value'])] = a['Value']['value'] }
-  end
+      super(new_params)
 
-  private
+      self.attributes ||= []
+      attributes.each { |a| self[methodify(a['AttributeID']['value'])] = a['Value']['value'] }
+    end
 
-  def methodify(word)
-    word.gsub(/(.)([A-Z])/,'\1_\2').downcase
+    private
+
+    def methodify(word)
+      word.gsub(/(.)([A-Z])/,'\1_\2').downcase
+    end
   end
 end
