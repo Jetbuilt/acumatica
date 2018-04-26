815e6d993578864917e2c6a256f5988af90466c3
Jared Moody <jared@jetbuilt.com>
2018-04-25 23:34:12 -0700

    Correctly handle transforming nested array and hash body parameters
    - Add specs for creating SalesOrder with array of Details
    - Allow passing query parameters when creating resource

--- lib/acumatica/resource.rb
+++ lib/acumatica/resource.rb
@@ -8,27 +8,20 @@ require "ostruct"
 module Acumatica
   class Resource < OpenStruct
     class << self
-      def create(attrs = {})
+      def create(body = {}, params = {})
         response = Acumatica::Client.instance.connection.put do |req|
           req.url url
-          req.body = format_params(attrs) if attrs
+          req.body = format_request_body(body) if body
+          req.params = parse_query_params(params) if params
         end
 
         new(response.body)
       end
 
-      def find_all(select: nil, filter: nil, expand: nil, offset: nil, limit: nil)
-        params = {}
-
-        params['$select'] = select if select
-        params['$filter'] = filter if filter
-        params['$expand'] = expand if expand
-        params['$skip']   = offset if offset
-        params['$top']    = limit  if limit
-
+      def find_all(params = {})
         response = Acumatica::Client.instance.connection.get do |req|
           req.url url
-          req.params = params
+          req.params = parse_query_params(params) if params
         end
 
         response.body.map { |attrs| new(attrs) }
@@ -40,10 +33,33 @@ module Acumatica
 
       private
 
-      def format_params(attrs)
-        attrs.transform_keys! { |key| key.to_s.camelize.gsub("Id", "ID") }
-        attrs.transform_values! { |value| value.is_a?(Hash) ? value : { "value" => value } }
-        attrs
+      def deep_transform_values!(object)
+        case object
+        when Array
+          object.map! { |e| deep_transform_values!(e) }
+        when Hash
+          object.each do |key, value|
+            object[key] = deep_transform_values!(value)
+          end
+        else
+          { "value" => object }
+        end
+      end
+
+      def format_request_body(body_hash)
+        body = body_hash.deep_transform_keys { |key| key.to_s.camelize.gsub("Id", "ID") }
+        deep_transform_values!(body)
+      end
+
+      def parse_query_params(params)
+        query_params = {}
+
+        query_params['$select'] = params[:select] if params[:select]
+        query_params['$filter'] = params[:filter] if params[:filter]
+        query_params['$expand'] = params[:expand] if params[:expand]
+        query_params['$skip']   = params[:offset] if params[:offset]
+        query_params['$top']    = params[:limit]  if params[:limit]
+        query_params
       end
     end
 
@@ -55,8 +71,8 @@ module Acumatica
     private
 
     def format_params(params)
-      params.transform_keys!   { |k| methodify(k) }
-      params.transform_values! { |v| v.is_a?(Hash) && v.keys == ["value"] ? v.values.first : v }
+      new_params = params.transform_keys { |k| methodify(k) }
+      new_params.transform_values! { |v| v.is_a?(Hash) && v.keys == ["value"] ? v.values.first : v }
     end
 
     def format_attributes!
