8639677d22fa9717f2f90a2f88ea11091268c1e2
Jared Moody <jared@jetbuilt.com>
2022-12-09 13:18:46 -0800

    Enable new rubocop cops

--- spec/support/client.rb
+++ spec/support/client.rb
@@ -2,8 +2,8 @@
 
 def configure_client
   Acumatica::Client.configure do |config|
-    config.url = ENV['ACUMATICA_URL']
-    config.name = ENV['ACUMATICA_USER']
-    config.password = ENV['ACUMATICA_PASSWORD']
+    config.url      = ENV.fetch('ACUMATICA_URL')
+    config.name     = ENV.fetch('ACUMATICA_USER')
+    config.password = ENV.fetch('ACUMATICA_PASSWORD')
   end
 end
